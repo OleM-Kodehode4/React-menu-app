b1b0b6d550ab5d79814e4a54e215e381af0ecf0f
OleM-Kodehode4 <olemathias.kodehode@gmail.com>
2025-01-11 00:27:33 +0100

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/React-menu-app/',  // Legg til dette for GitHub Pages
 })
